fcac505d498d4bb8b01b8d92d4f94d868b6a2dd6
zephyro24 <aust2997@gmail.com>
2026-08-05 21:40:50 -0600

    Project 7

---
+++ Projects/Project7/myscripts.js
@@ -0,0 +1,30 @@
+var imgArray = [
+	'image1Lg.png',
+	'image2Lg.png',
+	'image3Lg.png',
+	'image4Lg.png'
+];
+
+var titleArray = [
+	'Couple Biking', 
+	'Couple Scuba Diving', 
+	'Couple Snow Boarding', 
+	'Couple Flyfishing'
+];
+
+var imgPath = "images/FullSize/";
+
+function swapImage(imgID) {
+	var theImage = document.getElementById('theImage');
+	var textDiv  = document.getElementById('bottomText');
+
+	theImage.src = imgPath + imgArray[imgID];
+	textDiv.innerHTML = titleArray[imgID];
+}
+
+function preloadImages() {
+	for (var i = 0; i < imgArray.length; i++) {
+		var tmpImg = new Image();
+		tmpImg.src = imgPath + imgArray[i];
+	}
+}
